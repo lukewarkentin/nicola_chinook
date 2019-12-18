e060c5a544a3e6ddc12df7668dd9583953298e90
lukewarkentin <luke.warkentin@gmail.com>
2019-12-18 14:58:54 -0800

    Added Model 0b, updated figures and tables for WAIC and loo

--- model_comparison.R
+++ model_comparison.R
@@ -15,9 +15,9 @@ d <- read.csv("./data/model_data.csv")
 
 # Models to compare:
 
-# 0: Base model (ocean survival only)
 
 # Two beta models:
+# 0: Base model (ocean survival only)
 # 1: Base + spawning flows
 # 2: Base + fall flood
 # 3: Base + ice days
@@ -31,6 +31,7 @@ d <- read.csv("./data/model_data.csv")
 # 11: Full model with one beta
 
 # One beta models:
+# 0b: Base model (ocean survival only)
 # 1b: Base + spawning flows
 # 2b: Base + fall flood
 # 3b: Base + ice days
@@ -60,7 +61,8 @@ dat <- list(
 )
 
 # Declare initial values and parameters to track for each model --------
-# Base model------
+# Two beta models:---------
+# Model 0: Base model------
 inits_0= rep(
   list(
     list(lnalpha = runif(1, 0,3), 
@@ -71,9 +73,8 @@ inits_0= rep(
     )), 3
 )
 pars_track_0 <- c("alpha","betaW", "betaH","b1",
-                     "tau", "pp_log_RS",  "log_lik")
+                  "tau", "pp_log_RS",  "log_lik")
 
-# Two beta models:---------
 # Model 1---------
 inits_1= rep(
   list(
@@ -249,6 +250,18 @@ pars_track_11 <- c("alpha","betaW", "betaH","b1",
                      "tau", "pp_log_RS",  "log_lik")
 
 # One beta models--------
+# Model 0: Base model------
+inits_0b= rep(
+  list(
+    list(lnalpha = runif(1, 0,3), 
+         beta = rnorm(1, 0.0002, 0.0001),
+         b1 = rnorm(1, mean=0, sd=0.1),
+         tau =  runif(1, 0, 2)
+    )), 3
+)
+pars_track_0b <- c("alpha","beta","b1",
+                  "tau", "pp_log_RS",  "log_lik")
+
 # Model 1b---------
 inits_1b= rep(
   list(
@@ -425,10 +438,10 @@ pars_track_11b <- c("alpha", "beta","b1",
 
 
 # Fit models--------
-fit_ricker_0 <- stan( file = "ricker_linear_logRS_0_base.stan", 
-                         data=dat, chains=3, iter=10000, init=inits_0, 
-                         cores=2, pars=pars_track_0)
 # Two beta models
+fit_ricker_0 <- stan( file = "ricker_linear_logRS_0.stan", 
+                      data=dat, chains=3, iter=10000, init=inits_0, 
+                      cores=2, pars=pars_track_0)
 fit_ricker_1 <- stan( file = "ricker_linear_logRS_1.stan", 
                          data=dat, chains=3, iter=10000, init=inits_1, 
                          cores=2, pars=pars_track_1)
@@ -463,6 +476,9 @@ fit_ricker_11 <- stan( file = "ricker_linear_logRS_11.stan",
                        data=dat, chains=3, iter=10000, init=inits_11, 
                        cores=2, pars=pars_track_11)
 # One beta models
+fit_ricker_0b <- stan( file = "ricker_linear_logRS_0b.stan", 
+                      data=dat, chains=3, iter=10000, init=inits_0b, 
+                      cores=2, pars=pars_track_0b)
 fit_ricker_1b <- stan( file = "ricker_linear_logRS_1b.stan", 
                       data=dat, chains=3, iter=10000, init=inits_1b, 
                       cores=2, pars=pars_track_1b)
@@ -500,57 +516,90 @@ fit_ricker_11b <- stan( file = "ricker_linear_logRS_11b.stan",
 # Compare models ---------- 
 #fits <- ls(pattern="fit_ricker")
 
-waic_tab <- rethinking::compare(fit_ricker_0, fit_ricker_1, fit_ricker_2, fit_ricker_3, fit_ricker_4, fit_ricker_5, fit_ricker_6, fit_ricker_7, fit_ricker_8, fit_ricker_9, fit_ricker_10, fit_ricker_11,
-                                fit_ricker_1b, fit_ricker_2b, fit_ricker_3b, fit_ricker_4b, fit_ricker_5b, fit_ricker_6b, fit_ricker_7b, fit_ricker_8b, fit_ricker_9b, fit_ricker_10b, fit_ricker_11b  )
-png("./figures/fig_WAIC_model_compare.png", pointsize=20, width=500)
+waic_tab <- rethinking::compare(fit_ricker_0, fit_ricker_1, fit_ricker_2, fit_ricker_3, fit_ricker_4, fit_ricker_5, 
+                                fit_ricker_6, fit_ricker_7, fit_ricker_8, fit_ricker_9, fit_ricker_10, fit_ricker_11, 
+                                fit_ricker_0b, fit_ricker_1b, fit_ricker_2b, fit_ricker_3b, fit_ricker_4b, fit_ricker_5b,
+                                fit_ricker_6b, fit_ricker_7b, fit_ricker_8b, fit_ricker_9b, fit_ricker_10b, fit_ricker_11b)
+# Plot WIAC fig
+png("./figures/fig_WAIC_model_compare.png", pointsize=20, width=500, height=1000)
 plot(waic_tab, SE=TRUE, dSE=TRUE)
 dev.off()
+
+# Save WAIC table
 waic <- waic_tab@output
 waic$cumulative_weight <- cumsum(waic$weight)
-waic
-write.csv(waic, "WAIC_table.csv")
+row.names(waic) <- sub("fit_ricker_", "Model ", row.names(waic))
+# save WAIC table
+write.csv(round(waic,2), "WAIC_table.csv")
+
 
 # Compare parameter estimates - environmental variable effect estimates
 pars_plot <- c("b1","b2", "b3", "b4","b5")
 png("./figures/fig_parameter_estimates_model_compare.png", width=800, height=3000, pointsize = 18)
-plot(coeftab(fit_ricker_0, fit_ricker_1, fit_ricker_2, fit_ricker_3, fit_ricker_4, fit_ricker_5, fit_ricker_6, fit_ricker_7, fit_ricker_8, fit_ricker_9, fit_ricker_10, fit_ricker_11,
-             fit_ricker_1b, fit_ricker_2b, fit_ricker_3b, fit_ricker_4b, fit_ricker_5b, fit_ricker_6b, fit_ricker_7b, fit_ricker_8b, fit_ricker_9b, fit_ricker_10b, fit_ricker_11b), pars=pars_plot)
+plot(coeftab(fit_ricker_0, fit_ricker_1, fit_ricker_2, fit_ricker_3, fit_ricker_4, fit_ricker_5, 
+             fit_ricker_6, fit_ricker_7, fit_ricker_8, fit_ricker_9, fit_ricker_10, fit_ricker_11, 
+             fit_ricker_0b, fit_ricker_1b, fit_ricker_2b, fit_ricker_3b, fit_ricker_4b, fit_ricker_5b,
+             fit_ricker_6b, fit_ricker_7b, fit_ricker_8b, fit_ricker_9b, fit_ricker_10b, fit_ricker_11b), pars=pars_plot)
 dev.off()
 
 # beta terms - something not right here - coeftab doesn't match traceplot
 pars_plot_2 <- c("betaW", "betaH", "beta")
 png("./figures/fig_beta_estimates_model_compare.png", width=800, height=2000, pointsize = 18)
-plot(coeftab(fit_ricker_0, fit_ricker_1, fit_ricker_2, fit_ricker_3, fit_ricker_4, fit_ricker_5, fit_ricker_6, fit_ricker_7, fit_ricker_8, fit_ricker_9, fit_ricker_10, fit_ricker_11,
-             fit_ricker_1b, fit_ricker_2b, fit_ricker_3b, fit_ricker_4b, fit_ricker_5b, fit_ricker_6b, fit_ricker_7b, fit_ricker_8b, fit_ricker_9b, fit_ricker_10b, fit_ricker_11b), pars=pars_plot_2)
+plot(coeftab(fit_ricker_0, fit_ricker_1, fit_ricker_2, fit_ricker_3, fit_ricker_4, fit_ricker_5, 
+             fit_ricker_6, fit_ricker_7, fit_ricker_8, fit_ricker_9, fit_ricker_10, fit_ricker_11, 
+             fit_ricker_0b, fit_ricker_1b, fit_ricker_2b, fit_ricker_3b, fit_ricker_4b, fit_ricker_5b,
+             fit_ricker_6b, fit_ricker_7b, fit_ricker_8b, fit_ricker_9b, fit_ricker_10b, fit_ricker_11b, se=TRUE, digits=10), pars=pars_plot_2)
 dev.off()
 
 # Leave one out 
-models <-list(fit_ricker_0, fit_ricker_1, fit_ricker_2, fit_ricker_3, fit_ricker_4, fit_ricker_5, fit_ricker_6, fit_ricker_7, fit_ricker_8, fit_ricker_9, fit_ricker_10, fit_ricker_11,
-              fit_ricker_1b, fit_ricker_2b, fit_ricker_3b, fit_ricker_4b, fit_ricker_5b, fit_ricker_6b, fit_ricker_7b, fit_ricker_8b, fit_ricker_9b, fit_ricker_10b, fit_ricker_11b)
+models <-list(fit_ricker_0, fit_ricker_1, fit_ricker_2, fit_ricker_3, fit_ricker_4, fit_ricker_5, 
+              fit_ricker_6, fit_ricker_7, fit_ricker_8, fit_ricker_9, fit_ricker_10, fit_ricker_11, 
+              fit_ricker_0b, fit_ricker_1b, fit_ricker_2b, fit_ricker_3b, fit_ricker_4b, fit_ricker_5b,
+              fit_ricker_6b, fit_ricker_7b, fit_ricker_8b, fit_ricker_9b, fit_ricker_10b, fit_ricker_11b)
 loovals <- lapply(models, loo)
+loovals[[1]]
 plot(loovals[[8]]) # plot Pareto shape k value for each observation
 loo_mod_compare <- loo_compare(loovals)
 lootab <- as.data.frame(loo_mod_compare)
 #row.names(lootab) <- paste0("fit_ricker_", as.numeric(substr(row.names(lootab),6,8))-1) 
 row.names(lootab) <- as.numeric(substr(row.names(lootab),6,8))-1
-
-row.names(lootab) <- ifelse(as.numeric(row.names(lootab)) <= 11, row.names(lootab), paste0(as.numeric(row.names(lootab))-11, "b"))
+row.names(lootab) <- ifelse(as.numeric(row.names(lootab)) <= 11, row.names(lootab), paste0(as.numeric(row.names(lootab))-12, "b"))
+row.names(lootab) <- paste0("Model ", row.names(lootab))
 
 # save loo table
-write.csv(lootab, "./loo_comparison.csv")
+write.csv(round(lootab,2), "./loo_comparison.csv")
 
 # graph
 str(lootab)
-png("./figures/fig_LOO_model_compare.png", pointsize=20, width=500)
+png("./figures/fig_LOO_model_compare.png", pointsize=20, width=500, height=1000)
 par(mar=c(4,6,0,0)+0.1)
-plot(x=lootab$elpd_loo, y=1:nrow(lootab), yaxt='n', ylab="", xlab="ELPD LOO", xlim=c(-33, -27))
-axis(2, at=seq_len(nrow(lootab)), labels = rownames(lootab), las=2)
-segments(x0=lootab$elpd_loo - lootab$se_elpd_loo, x1=lootab$elpd_loo + lootab$se_elpd_loo, y0=1:nrow(lootab))
+plot(x=lootab$elpd_loo, y=rev(1:nrow(lootab)), yaxt='n', ylab="", xlab="ELPD LOO", xlim=c(min(lootab$elpd_loo - lootab$se_elpd_loo), max(lootab$elpd_loo + lootab$se_elpd_loo)))
+axis(2, at=seq_len(nrow(lootab)), labels = rev(rownames(lootab)), las=2)
+segments(x0=lootab$elpd_loo - lootab$se_elpd_loo, x1=lootab$elpd_loo + lootab$se_elpd_loo, y0=rev(1:nrow(lootab)))
 abline(v=max(lootab$elpd_loo))
-points(x=max(lootab$elpd_loo) + lootab$elpd_diff, y=1:nrow(lootab)+0.5, pch=2, col="gray")
-segments(x0=lootab$elpd_loo - lootab$se_diff, x1=lootab$elpd_loo + lootab$se_diff, y0=1:nrow(lootab) +0.5, col="gray")
+points(x=max(lootab$elpd_loo) + lootab$elpd_diff, y=rev(1:nrow(lootab))+0.5, pch=2, col="gray")
+segments(x0=lootab$elpd_loo - lootab$se_diff, x1=lootab$elpd_loo + lootab$se_diff, y0=rev(1:nrow(lootab)) +0.5, col="gray")
 dev.off()
 
+
+# Check R2 of the models
+# get posterior estimates for log(R/S)
+post_list <- lapply(X=models, FUN=as.data.frame) # make list of posterior data frames
+ppd_list <- lapply(post_list, FUN=function(i) { i[, grep("pp_log_RS", colnames(i))] }) # select only posteriors for predicted log(R/S)
+mn_pp_log_RS <- lapply(ppd_list, function(i) { apply(i, 2, mean)}) # get means of predicted log(R/S)
+preds <- as.data.frame(t(do.call(rbind, mn_pp_log_RS))) # bind all means for each observation into one data frame
+mods <- paste0("Model ", c(0:11, paste0(0:11, "b"))) # make a vector of model names
+names(preds) <- mods # name predictions after models
+obs_log_RS <- log(d$wild_recruits / d$ total_spawners) # get observed log(R/S)
+rsq <- apply(preds, 2, function(i) {cor(obs_log_RS,i)^2}) # get r-squared for each model
+rsqs <-sort(rsq) # sort largest to smallest
+# Plot R-squared values
+png("./figures/fig_Rsquared_model_compare.png", pointsize=20, width=800, height=800)
+par(mar=c(4,6,0,0)+0.1)
+plot(x=rsqs, y= 1:length(rsqs), xlab="R-squared", yaxt="n", ylab="",  type="p")
+axis(2, at=seq_len(length(rsqs)), labels = names(rsqs), las=2)
+text(x=rsqs, y= 1:length(rsqs)+0.4, label=round(rsqs,2), cex=0.7)
+dev.off()
 
 #plot(x=1:18, y=1:18, pch=1:18)
 
--- ricker_linear_logRS_0b.stan
+++ ricker_linear_logRS_0b.stan
@@ -3,21 +3,19 @@ data {
   vector[N] log_RS; // log of wild recruits per spawner
   vector[N] S; // total spawners
   vector[N] ocean_surv; // smolt to age 3 survival
-  vector[N] aug_mean_flow_rear; // mean aug flow in rearing year
   }
 
 parameters {
-  real lnalpha; // log(alpha) term  
+  real lnalpha; // log(alpha) term
   real beta; // beta for total spawners
   real b1; // effect of ocean survival 
-  real b5; // effect of mean aug flow during rearing - rearing
   real tau; // error term
   }
 
 transformed parameters {
   vector[N] pred_log_RS; // predicted log(recruits/spawner)
   real alpha; // productivity
-  pred_log_RS = lnalpha -beta * S  + b1 * ocean_surv + b5 * aug_mean_flow_rear ; // model predicting recruits from alpha and beta and covariates
+  pred_log_RS = lnalpha - beta * S + b1 * ocean_surv; // model predicting recruits from alpha and beta and covariates
   alpha = exp(lnalpha); // exponentiate alpha
   }
 
@@ -26,7 +24,6 @@ model {
   lnalpha ~ uniform(0,3); // prior for alpha truncated at 0: maximum number of recruits per spawner at low stock size OR slope of line at origin
   beta ~ uniform(0,10); // prior for beta: rate of decrease - normal distribution had trouble getting to small value of beta
   b1 ~ normal(0, 1); // prior for effect of ocean survival
-  b5 ~ normal(0, 1); // prior for effect of aug mean flow rearing
   tau ~ gamma(0.01, 0.01); // prior error
   }
 
