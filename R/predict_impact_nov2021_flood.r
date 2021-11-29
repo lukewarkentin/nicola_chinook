f9978dd7e9194adeb23dc90f485bd078ff4118ea
lukewarkentin <luke.warkentin@gmail.com>
2021-11-29 11:04:33 -0800

    update spawner numbers and rerun

--- R/predict_impact_nov2021_flood.r
+++ R/predict_impact_nov2021_flood.r
@@ -1,20 +1,6 @@
-library(tidyhydat)
-library(bayesplot)
 library(dplyr)
 library(ggplot2)
-library(rstan)
-library(purrr)
-library(stringr)
-library(lubridate)
-library(readxl)
-library(RColorBrewer)
-devtools::install_github("cran/DMwR")
-library(DMwR)
 library(tidyr)
-library(EflowStats)
-devtools::install_github("rmcelreath/rethinking")
-library(rethinking)
-# rstan_options(auto_write=TRUE)
 
 rm(list=ls())
 
@@ -27,7 +13,12 @@ d_unscaled <- read.csv("./data_out/model_data_unscaled.csv")
 # read in data frame of posterior samples of model parameters from model stacking
 post <- read.csv("data_out/model_stacking_posterior_samples.csv")
 
-options(scipen = 12) # adjust so estimates are in fixed notation
+# Set total spawners, hatchery spawners and wild spawners
+# Data from Chuck Parken 2021-11-29
+total_spawners <- 4035
+wild_spawners <- 1089
+hatchery_spawners <- 2946
+
 
 # Write function to propagate uncertainty in all parameters to get recruits with different inputs 
 # Note, if we start adding more scenarios, probably best to make a data frame with 
@@ -50,8 +41,8 @@ faux_scale <- function(x, scale_by) {
 }
 
 # check betas - use not fixed version when including all three beta terms because they are from a model stacked average
-plot(density(post$beta))
-lines(density(post$beta_fix), col="green")
+#plot(density(post$beta))
+#lines(density(post$beta_fix), col="green")
 
 # get flow data
 # UNapproved data downloaded from WSC website Nov 26, 2021 https://wateroffice.ec.gc.ca/download/index_e.html?results_type=real_time
@@ -64,20 +55,12 @@ fdm <- fd %>% group_by(date) %>% summarise(mean_flow = mean(flow_cms))
 fdm$month <- month(fdm$date)
 augflowspawn <- mean(fdm$mean_flow[fdm$month==8])
 
-max(d_unscaled$sep_dec_max_flow)
-mean(d_unscaled$aug_mean_flow)
-
 # get recruits for average flood
 avg_flood_1992_2013 <- get_recruits(alpha=post$alpha, beta_t = post$beta, beta_w = post$betaW, beta_h=post$betaH,
              b1 = post$b1, b2 = post$b2, b3 = post$b3, b4=post$b4, b5=post$b5,
-             # Data from Chuck Parken 2021-11-26
-             # total spawners (rough) = 4,000 with 439 natural and 3,561 hatchery. 
-             # the hatchery vs natural is from the tag application sample and 
-             # normally we use the carcass sample because it is more representative. 
-             # i can't find those data yet, but i have asked Sara for it.
-             total_spawners = 4000, wild_spawners = 439, hatchery_spawners = 3561,
+             total_spawners = total_spawners, wild_spawners = wild_spawners, hatchery_spawners = hatchery_spawners,
              # smolt survival for the most recent completed cohort (2015) is 0.01999491
-             smoltsurv= faux_scale(0.01999491,d_unscaled$smolt_age3_survival),
+              smoltsurv= faux_scale(0.01999491,d_unscaled$smolt_age3_survival),
              # from unapproved WSC data, which is 5.2, below the average used to fit data, which was 8.6
              spawnflow = faux_scale(augflowspawn,d_unscaled$aug_mean_flow),
              flood = 0, # average flood
@@ -88,12 +71,7 @@ avg_flood_1992_2013 <- get_recruits(alpha=post$alpha, beta_t = post$beta, beta_w
 # get recruits for max observed fall flood from data set used to fit model
 max_flood_1992_2013 <- get_recruits(alpha=post$alpha, beta_t = post$beta, beta_w = post$betaW, beta_h=post$betaH,
              b1 = post$b1, b2 = post$b2, b3 = post$b3, b4=post$b4, b5=post$b5,
-             # Data from Chuck Parken 2021-11-26
-             # total spawners (rough) = 4,000 with 439 natural and 3,561 hatchery. 
-             # the hatchery vs natural is from the tag application sample and 
-             # normally we use the carcass sample because it is more representative. 
-             # i can't find those data yet, but i have asked Sara for it.
-             total_spawners = 4000, wild_spawners = 439, hatchery_spawners = 3561,
+             total_spawners = total_spawners, wild_spawners = wild_spawners, hatchery_spawners = hatchery_spawners,
              # smolt survival for the most recent completed cohort (2015) is 0.01999491
              smoltsurv= faux_scale(0.01999491,d_unscaled$smolt_age3_survival),
              # from unapproved WSC data, which is 5.2, below the average used to fit data, which was 8.6
@@ -106,12 +84,7 @@ max_flood_1992_2013 <- get_recruits(alpha=post$alpha, beta_t = post$beta, beta_w
 # get recruits for Nov fall flood = 367 cms, max recorded value before station failure # see email from Rich McCleary
 est_nov21_flood_367cms <- get_recruits(alpha=post$alpha, beta_t = post$beta, beta_w = post$betaW, beta_h=post$betaH,
                                        b1 = post$b1, b2 = post$b2, b3 = post$b3, b4=post$b4, b5=post$b5,
-                                       # Data from Chuck Parken 2021-11-26
-                                       # total spawners (rough) = 4,000 with 439 natural and 3,561 hatchery. 
-                                       # the hatchery vs natural is from the tag application sample and 
-                                       # normally we use the carcass sample because it is more representative. 
-                                       # i can't find those data yet, but i have asked Sara for it.
-                                       total_spawners = 4000, wild_spawners = 439, hatchery_spawners = 3561,
+                                       total_spawners = total_spawners, wild_spawners = wild_spawners, hatchery_spawners = hatchery_spawners,
                                        # smolt survival for the most recent completed cohort (2015) is 0.01999491
                                        smoltsurv= faux_scale(0.01999491,d_unscaled$smolt_age3_survival),
                                        # from unapproved WSC data, which is 5.2, below the average used to fit data, which was 8.6
@@ -124,12 +97,7 @@ est_nov21_flood_367cms <- get_recruits(alpha=post$alpha, beta_t = post$beta, bet
 # get recruits for Nov fall flood = 762 cms # see email from Rich McCleary
 est_nov21_flood_762cms <- get_recruits(alpha=post$alpha, beta_t = post$beta, beta_w = post$betaW, beta_h=post$betaH,
              b1 = post$b1, b2 = post$b2, b3 = post$b3, b4=post$b4, b5=post$b5,
-             # Data from Chuck Parken 2021-11-26
-             # total spawners (rough) = 4,000 with 439 natural and 3,561 hatchery. 
-             # the hatchery vs natural is from the tag application sample and 
-             # normally we use the carcass sample because it is more representative. 
-             # i can't find those data yet, but i have asked Sara for it.
-             total_spawners = 4000, wild_spawners = 439, hatchery_spawners = 3561,
+             total_spawners = total_spawners, wild_spawners = wild_spawners, hatchery_spawners = hatchery_spawners,
              # smolt survival for the most recent completed cohort (2015) is 0.01999491
              smoltsurv= faux_scale(0.01999491,d_unscaled$smolt_age3_survival),
              # from unapproved WSC data, which is 5.2, below the average used to fit data, which was 8.6
